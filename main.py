338089b31031d7f5524edb6c2a5a74e6943e78b2
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-02 14:32:50 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+CSC111 Project: University of Toronto Course Finder: Main
+
+Module Description:
+====================
+The module contains the function calls to utilize our other modules to run our pygame application
+that will display the corresponding trees.
+"""
