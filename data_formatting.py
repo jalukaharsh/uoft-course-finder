6d6d3621194a0b62c18e40c95603ae10f87206a7
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-02 14:29:51 -0400

    Add files via upload
    Created data_formatting.py

---
+++ data_formatting.py
@@ -0,0 +1,7 @@
+"""CSC111 Project: University of Toronto Course Finder: Data Formatting
+
+Module Description:
+====================
+The module contains the function that processes the raw data file into list of courses, with each
+course represented as a dictionary.
+"""
