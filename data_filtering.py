c46542fb61070a6bca46df6b53ceac56906d50e6
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-02 14:43:09 -0400

    Created data_filtering.py

---
+++ data_filtering.py
@@ -0,0 +1,8 @@
+"""
+CSC111 Project: University of Toronto: Data Filtering
+
+Module Description:
+====================
+The module contains the code that will filter the courses based on courses taken in the past,
+breadth, development and level.
+"""
