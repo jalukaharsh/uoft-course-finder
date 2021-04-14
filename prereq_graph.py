b5780d6d641e736bca633a9eb5336f30b76e60a0
dniloy <dihan8917@gmail.com>
2021-04-15 01:13:13 +0600

    Update prereq_graph.py

--- prereq_graph.py
+++ prereq_graph.py
@@ -2,16 +2,32 @@ from typing import List
 from data_formatting import Graph
 
 
-def build_prereq_graph(course_graph: Graph(), course: str) -> Graph():
+def build_prereq_graph(courses: Dict[str, Dict], course: str):
     """Returns the prereq/coreq trace subgraph of the given course."""
     v1 = course_graph.get_vertex(course)
-    prereq_graphs = []
+    neighbours = courses[course]['neighbours']
 
-    for neighbour in v1:
-        edge_type = v1.get_edge_type(neighbour)
-        if edge_type in {'prereq', 'coreq'}:
-            prereq_graphs.append(build_prereq_graph(course_graph, neighbour.name))
-    merge_graphs(prereq_graphs)
+    if neighbours == []:  # base case: no prereqs/coreqs
+        graph = Graph()
+        graph.add_vertex(v1)
+        return graph
+    else:
+        prereq_graphs = []
+        for neighbour in neighbours:
+            prereq_graphs.append(build_prereq_graph(courses, neighbour))
+        merge_graphs(prereq_graphs)
+
+
+# def build_prereq_graph(course_graph: Graph(), course: str) -> Graph():
+#     """Returns the prereq/coreq trace subgraph of the given course."""
+#     v1 = course_graph.get_vertex(course)
+#     prereq_graphs = []
+#
+#     for neighbour in v1:
+#         edge_type = v1.get_edge_type(neighbour)
+#         if edge_type in {'prereq', 'coreq'}:
+#             prereq_graphs.append(build_prereq_graph(course_graph, neighbour.name))
+#     merge_graphs(prereq_graphs)
 
 
 def merge_graphs(graphs: List[Graph()]) -> Graph():
