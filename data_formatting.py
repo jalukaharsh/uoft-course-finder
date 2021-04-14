950d33ea1171b94ad015980d9380a4157a9c2e8f
shaulfrancus <56957592+shaulfrancus@users.noreply.github.com>
2021-04-14 16:15:39 -0400

    i think i finished prereqtree init

--- data_formatting.py
+++ data_formatting.py
@@ -7,7 +7,7 @@ course represented as a dictionary.
 """
 from __future__ import annotations
 import json
-from typing import Any
+from typing import Any, Optional
 import re
 
 
@@ -30,25 +30,71 @@ class PrereqTree:
         - subtrees: The vertices that are adjacent to this vertex. This
 
     Representation Invariants:
-        - kind matches this regex \\A(?:(?:[A-Z]{3}[0-9]{3}[H,Y]1)|(?:or)|(?:and))\\Z
+        - item is 'and' or 'or' or of the form [A-Z]{3}[0-9]{3}[H,Y]1
     """
-    kind: str
-    subtrees: PrereqTree
-    
-    __init__(self, prereq_str: str) -> None:
-    
-    
+    item: str
+    subtrees: Optional[list[PrereqTree]]
 
+    def __init__(self, prereq_str: str) -> None:
+        # remove all whitespace
+        prereq_str = re.sub(r'\s+', prereq_str)
+        prereq_str = prereq_str.replace(';', ',')
 
+        # deal base case of prereq_str being just a single course:
+        if re.fullmatch(r'[A-Z]{3}[0-9]{3}[H,Y]1', prereq_str) is not None:
+            self.item = prereq_str
+            self.subtrees = None
+            return
 
-class _CoursesVertex:
-    """A vertex in a the CourseGraph representing a course
-     
-     Attributes:
-        - item: A course code
-        - prerequisites: A PrereqTree representing the prerequisites of the course
-        - corequisites: A PrereqTree representing the corequisites of the course
-    """
-    item: Any
-    prerequisites: PrereqTree
-    corequisites: PrereqTree
+        # tokenize the prereq string into the form of courses, commas,
+        # forward slashes and parentheses
+        split_str = re.findall(r'(?:(?:[A-Z]{3}[0-9]{3}[H,Y]1)|[/,\,, (, )])')
+
+        # combine each parenthetical into a single string (which we will later recurse on)
+        while '(' in split_str:
+            first_paren = split_str.index('(')
+            nest = 1
+            length = 1
+            while nest >= 1:
+                i = length + first_paren
+                char = split_str[i]
+                if char == '(':
+                    nest += 1
+                elif char == ')':
+                    nest -= 1
+
+                length += 1
+            
+            # join the parenthesized string
+            parenthesized_str = ''.join(split_str[first_paren:first_paren + length])
+            
+            if re.search(r'[A-Z]{3}[0-9]{3}[H,Y]1', parenthesized_str) is None:
+                # if the parenthesized string includes no course codes then we remove it
+                split_str = split_str[0: first_paren] + split_str[first_paren + length:]
+            else:
+                split_str = split_str[0: first_paren] + [parenthesized_str] + \
+                            split_str[first_paren + length:]
+
+        ors = []  # list of each or(/) block. if longer than one then
+        current = []
+        for i in range(0, len(split_str)):
+            char = split_str[i]
+            if char == ',':
+                ors.append(current)
+                current = []
+            elif char != '/':
+                current.append(char)
+
+        if current != '':
+            ors.append(current)
+
+        if len(ors) == 1:
+            self.item = 'or'
+            self.subtrees = []
+            for el in ors[0]
+                self.subtrees.append(PrereqTree(el))
+        else:
+            self.item = 'and'
+            self.subtrees = []
+            for el in ors:
+                self.subtrees.append(PrereqTree(el))
