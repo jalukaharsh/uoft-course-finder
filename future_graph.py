c1cb63adce53e200d3c736228076467d86f16721
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-15 11:15:34 -0400

    Made changes to colours of nodes

--- future_graph.py
+++ future_graph.py
@@ -7,7 +7,7 @@ from typing import Dict
 def future(courses: Dict[str, Dict], course: str) -> nx.Graph():
     """Return a graph containing all the future courses that this course can lead to. """
     future_graph = nx.DiGraph()
-    future_graph.add_node(course)
+    future_graph.add_node(course, tag='original')
     add_children(courses, course, future_graph)
     return future_graph
 
@@ -16,7 +16,7 @@ def add_children(courses: Dict[str, Dict], course: str, graph: nx.Graph()) -> No
     """Mutate the given graph object to add all the courses that course is a prerequisite for. """
     for item in courses:
         if courses[item]['prerequisites'] is not None and course in courses[item]['prerequisites']:
-            graph.add_node(item)
+            graph.add_node(item, tag='no')
             graph.add_edge(course, item)
             try:
                 add_children(courses, item, graph)
@@ -33,6 +33,7 @@ def future_one_level(courses: Dict[str, Dict], course: str) -> nx.Graph():
         if courses[item]['prerequisites'] is not None and course in courses[item]['prerequisites']:
             future_graph.add_node(item)
             future_graph.add_edge(course, item)
+    return future_graph
 
 
 def future_one_level_mutate(courses: Dict[str, Dict], course: str, future_graph: nx.Graph()) -> None:
