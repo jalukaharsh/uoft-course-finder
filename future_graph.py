cdb112f8eb65b73aa5550044a51dc051b328f87a
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-15 10:54:07 -0400

    Added one level update functions

--- future_graph.py
+++ future_graph.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 def future(courses: Dict[str, Dict], course: str) -> nx.Graph():
     """Return a graph containing all the future courses that this course can lead to. """
-    future_graph = nx.Graph()
+    future_graph = nx.DiGraph()
     future_graph.add_node(course)
     add_children(courses, course, future_graph)
     return future_graph
@@ -15,7 +15,33 @@ def future(courses: Dict[str, Dict], course: str) -> nx.Graph():
 def add_children(courses: Dict[str, Dict], course: str, graph: nx.Graph()) -> None:
     """Mutate the given graph object to add all the courses that course is a prerequisite for. """
     for item in courses:
-        if course in courses[item]['prerequisites']:
+        if courses[item]['prerequisites'] is not None and course in courses[item]['prerequisites']:
             graph.add_node(item)
-            graph.add_edge(item, course)
-            add_children(courses, item, graph)
+            graph.add_edge(course, item)
+            try:
+                add_children(courses, item, graph)
+            except RecursionError:
+                pass
+
+
+def future_one_level(courses: Dict[str, Dict], course: str) -> nx.Graph():
+    """Return a graph containing only the courses that the input course directly leads to.
+    """
+    future_graph = nx.Graph()
+    future_graph.add_node(course)
+    for item in courses:
+        if courses[item]['prerequisites'] is not None and course in courses[item]['prerequisites']:
+            future_graph.add_node(item)
+            future_graph.add_edge(course, item)
+
+
+def future_one_level_mutate(courses: Dict[str, Dict], course: str, future_graph: nx.Graph()) -> None:
+    """Mutate a graph to add only the courses that the input course directly leads to.
+
+    Preconditions:
+        - course is in the graph
+    """
+    for item in courses:
+        if courses[item]['prerequisites'] is not None and course in courses[item]['prerequisites']:
+            future_graph.add_node(item)
+            future_graph.add_edge(course, item)
