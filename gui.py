c87bd634411115ac9e41073bbec2a10e5213d8ec
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-15 16:44:18 -0400

    Added advanced filters for future graphs

--- gui.py
+++ gui.py
@@ -3,13 +3,35 @@
 import tkinter as tk
 from visualizing_graph import future_run
 from data_formatting import get_courses_data
-from prereq_graph import prereq_run
+# from prereq_graph import prereq_run
+from tkinter import ttk
+from data_filtering import filter_courses
 
 courses = get_courses_data()
+departments = list({courses[course]['department'] for course in courses})
+departments.sort()
+levels = ['100-level', '200-level', '300-level', '400-level']
+breadths = ['(1) Creative and Cultural Representation',
+            '(2) Thought, Belief and Behaviour',
+            '(3) Society and its Institutions',
+            '(4) Living Things and Their Environment',
+            '(5) The Physical and Mathematical Universes']
 
 
 def retrieve() -> None:
     """Ran whenever 'Search' button is clicked on the main screen. """
+    br = breadth_list.get()
+    # print(br)
+
+    lvl = level_list.get()
+    lvl = lvl[0]
+    # print(lvl)
+
+    department = dept_filter.get()
+    # print(department)
+
+    future_courses_dict = filter_courses(courses, lvl, department, br)
+
     newroot = tk.Tk()
     newroot.geometry("300x100")
 
@@ -18,11 +40,13 @@ def retrieve() -> None:
 
     if course_input.get() in courses:
         prereq_button = tk.Button(newframe, text="Prerequisite Graph",
-                                  command=lambda: prereq_run(course_input.get()))
+                                  # command=lambda: prereq_run(course_input.get())
+                                  )
         prereq_button.pack(side=tk.LEFT, padx=5, pady=20)
 
         future_button = tk.Button(newframe, text="Future Graph",
-                                  command=lambda: future_run(course_input.get()))
+                                  command=lambda: future_run(future_courses_dict,
+                                                             course_input.get()))
         future_button.pack(padx=5, pady=20)
     else:
         show_error = tk.Label(newframe, text='Sorry, no such course exists in the database. '
@@ -33,7 +57,7 @@ def retrieve() -> None:
 
 
 root = tk.Tk()
-root.geometry("600x450")
+root.geometry("300x200")
 
 frame = tk.Frame(root)
 frame.pack()
@@ -47,7 +71,25 @@ label.pack()
 course_input = tk.Entry(frame, width=20)
 course_input.pack(padx=5, pady=5)
 
+advanced = tk.StringVar()
+advanced.set("Advanced filters (for the future graph):")
+
+advanced_filters = tk.Label(frame, textvariable=advanced, pady=5)
+advanced_filters.pack()
+
+breadth_list = ttk.Combobox(frame, values=list(breadths), width=40)
+breadth_list.set('Pick a breadth category')
+breadth_list.pack()
+
+level_list = ttk.Combobox(frame, values=list(levels), width=40)
+level_list.set('Pick a level')
+level_list.pack()
+
+dept_filter = ttk.Combobox(frame, values=list(departments), width=40)
+dept_filter.set('Pick a department')
+dept_filter.pack()
+
 Button = tk.Button(frame, text="Search", command=retrieve)
-Button.pack(padx=5, pady=5)
+Button.pack(padx=5, pady=10)
 
 root.mainloop()
