6c88d54507ac5c0cd798d15f410295d6ba369aee
jalukaharsh <75231409+jalukaharsh@users.noreply.github.com>
2021-04-15 23:47:03 -0400

    One visualizing function for both graphs, new run
    new run function for prereq graph

--- visualizing_graph.py
+++ visualizing_graph.py
@@ -8,7 +8,8 @@ from typing import Dict
 import networkx as nx
 import plotly.graph_objects as go
 from future_graph import future
-
+from data_formatting import get_courses_data
+from prereq_graph import build_trace_graph
 
 LINE_COLOUR = 'rgb(210,210,210)'
 VERTEX_BORDER_COLOUR = 'rgb(50, 50, 50)'
@@ -65,62 +66,25 @@ def draw_graph(graph: nx.Graph(), layout: str = 'spring_layout') -> None:
     fig.show()
 
 
-def draw_graph_prereq(graph: nx.Graph(), layout: str = 'spring_layout') -> None:
-    """Return a visual interactive representation of the input graph.
-    """
-    pos = getattr(nx, layout)(graph)
-
-    x_values = [pos[k][0] for k in graph.nodes]
-    y_values = [pos[k][1] for k in graph.nodes]
-    labels = list(graph.nodes)
-
-    x_edges = []
-    y_edges = []
-    for edge in graph.edges:
-        x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
-        y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
-
-    trace3 = go.Scatter(x=x_edges,
-                        y=y_edges,
-                        mode='lines',
-                        name='edges',
-                        line=dict(color=LINE_COLOUR, width=1),
-                        hoverinfo='none',
-                        )
-
-    trace4 = go.Scatter(x=x_values,
-                        y=y_values,
-                        mode='markers',
-                        name='nodes',
-                        marker=dict(symbol='circle-dot',
-                                    size=5,
-                                    line=dict(color=VERTEX_BORDER_COLOUR, width=0.5)
-                                    ),
-                        text=labels,
-                        hovertemplate='%{text}',
-                        hoverlabel={'namelength': 0}
-                        )
-
-    data1 = [trace3, trace4]
-    fig = go.Figure(data=data1)
-    fig.update_layout({'showlegend': False})
-    fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
-    fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
-
-    fig.show()
-
-
 def future_run(courses: Dict, course: str) -> None:
     """Return a visualization of the future graph of the given course. """
     graph = future(courses, course)
     draw_graph(graph)
 
 
+def prereq_run(course: str) -> None:
+    """Return a visualization of the prerequisites graph of the given course. """
+    courses = get_courses_data()
+    g = build_trace_graph(courses, course)
+    draw_graph(g)
+
+
 if __name__ == '__main__':
     import python_ta
 
     python_ta.check_all(config={
-        'extra-imports': ['networkx', 'plotly.graph_objects', 'future_graph'],
+        'extra-imports': ['networkx', 'plotly.graph_objects', 'future_graph', 'prereq_graph',
+                          'data_formatting'],
         'allowed-io': [],
         'max-line-length': 100,
         'disable': ['E1136']
